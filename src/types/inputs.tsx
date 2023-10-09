87e51311bc5236a2b767ef8fbad081b3ce54b8fd
Javier Perez <125394473+Javier0003@users.noreply.github.com>
2023-10-09 17:09:04 +0200

    new type and changed too

--- src/types/inputs.tsx
+++ src/types/inputs.tsx
@@ -2,6 +2,12 @@ export type inputsProps = {
     values: string,
 }
 
-export type btnProps = {
+export type operatorProps = {
     addNumber: (value:string) => void,
+    removeNumber: () => void
+    solve: () => void
+}
+
+export type btnPropsKeyboard = {
+    addNumber: (value:string) => void
 }
