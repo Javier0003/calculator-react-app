75c116657d4c2b1df9aa1bbea8453cc183e05d7d
Javier Perez <125394473+Javier0003@users.noreply.github.com>
2023-10-10 15:35:27 +0200

    added new function type

--- src/types/inputs.tsx
+++ src/types/inputs.tsx
@@ -6,6 +6,7 @@ export type operatorProps = {
     addNumber: (value:string) => void,
     removeNumber: () => void
     solve: () => void
+    addOperator: (value:string) => void
 }
 
 export type btnPropsKeyboard = {
