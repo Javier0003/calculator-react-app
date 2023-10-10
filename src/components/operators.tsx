61312543ba5f226b66c1d6ad7a7e29b03946ab49
Javier Perez <125394473+Javier0003@users.noreply.github.com>
2023-10-10 15:35:17 +0200

    added operator css file

--- src/components/operators.tsx
+++ src/components/operators.tsx
@@ -1,54 +1,48 @@
-
-import styles from "../styles/buttonsOperators.module.css"
+import styles from "../styles/buttonsOperators.module.css";
 import { operatorProps } from "../types/inputs";
 const btns = ["÷", "x", "-", "+"];
 
-export function Operators({addNumber, removeNumber, solve}: operatorProps) {
-    const handleClick = (event: string) => {
-        const value = event
-        addNumber(value)
-    }
-    const handleRemove = () => {
-        removeNumber()
-    }
-    const handleEqual = () => {
-        solve()
-    }
-    return(
-        <div style={{
-            border: '1px solid black',
-            height: "78%",
-            width: "28.65%",
-            borderRadius: "0px 0px 20px 0px"
-        }}>
-            <div style={{
-                flexWrap: "wrap"
-            }}>
-                <button
-                className={styles.button}
-                onClick={handleRemove}
-                >
-                    <img src="../imgs\keyboard_backspace_FILL0_wght400_GRAD0_opsz24.svg" alt="" />
-                </button>
-                {btns.map(btns =>{
-                    return(
-                        <button className={styles.button}
-                            key={btns}
-                            onClick={() => {handleClick(btns)}}
-                            >
-                            {btns}
-                        </button>
-                    )})}
-                <button style={{
-                    border: 'none',
-                    width: '100%',
-                    height: '58px',
-                    borderRadius: "0px 0px 20px 0px"
-                }}
-                onClick={() => {handleEqual()}}
-                >=</button>
-            </div>
-            
-        </div>
-    )
+export function Operators({ addOperator, removeNumber, solve }: operatorProps) {
+  const handleClick = (event: string) => {
+    const value = event;
+    addOperator(value);
+  };
+  const handleRemove = () => {
+    removeNumber();
+  };
+  const handleEqual = () => {
+    solve();
+  };
+  return (
+    <div className={styles.container}>
+      <div style={{ flexWrap: "wrap" }}>
+        <button className={styles.button} onClick={handleRemove}>
+          <img
+            src="../imgs\keyboard_backspace_FILL0_wght400_GRAD0_opsz24.svg"
+          />
+        </button>
+        {btns.map((btns) => {
+          return (
+            <button
+              className={styles.button}
+              key={btns}
+              onClick={() => {
+                handleClick(btns);
+              }}
+            >
+              {btns}
+            </button>
+          );
+        })}
+        <button
+          className={styles.borderBtn}
+          onClick={() => {
+            handleEqual();
+          }}
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
 }
