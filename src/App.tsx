bf4c2c349e8fcfa944d8ee98713512529bc9a897
Javier Perez <125394473+Javier0003@users.noreply.github.com>
2023-10-10 15:38:21 +0200

    new functions and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,72 @@
-import { useState } from "react"
-import { Screen } from './components/screen'
-import { Keyboard } from './components/keyboard'
-import { Operators } from "./components/operators"
+import { useState } from "react";
+import { Screen } from "./components/screen";
+import { Keyboard } from "./components/keyboard";
+import { Operators } from "./components/operators";
+import styles from "./styles/main.module.css";
 
-const cantStart = [",", "0", "+", "x", "/", "÷"]
-const cantRepeat = ["÷", "x", "-", "+"]
+const cantStart = [",", "0", "+", "x", "/", "÷"];
+const cantRepeat = ["÷", "+"];
 
 function App() {
-  const [state, setState] = useState<string>("")
+  const [state, setState] = useState<string>("");
+  const [hiddenState, setHiddenState] = useState<string>("");
+  const [justCalculated, setJustCalculated] = useState<boolean>(false);
   const addNumber = (e: string) => {
-    if(state === "" && cantStart.includes(e)) return
-    if(cantRepeat.includes(e) && state.slice(-1) === e) return
-    if(e === "x"){
-      setState(state + "*")
-      return
+    if (justCalculated === true) {
+      setHiddenState("");
+      setState("");
+      setJustCalculated(false);
     }
-    if(e === "÷"){
-      setState(state + "/")
-      return
+
+    if (state === "" && cantStart.includes(e)) return;
+    setHiddenState(hiddenState + e);
+    setState(state + e);
+  };
+
+  const addOperator = (e: string) => {
+    if (state === "" && cantStart.includes(e)) return;
+    if (cantRepeat.includes(e) && state.slice(-1) === e) return;
+    if (state.slice(-1) === "x" && state.slice(-2) === "x") return;
+
+    if (e === "x") {
+      setHiddenState(state + "*");
     }
-    setState(state + e)
-  }
+
+    if (e === "÷") {
+      setHiddenState(state + "/");
+    }
+
+    if (e !== "÷" && e !== "x") {
+      setHiddenState(hiddenState + e);
+    }
+
+    setState(state + e);
+  };
+
   const removeNumber = () => {
-    setState(state.slice(0, -1))
-  }
+    setState(state.slice(0, -1));
+    setHiddenState(hiddenState.slice(0, -1));
+  };
+
   const solve = () => {
-    setState(eval(state))
-  }
+    setJustCalculated(true);
+    setState(eval(hiddenState).toString());
+    setHiddenState(eval(hiddenState).toString());
+  };
+
   //last thing we are on is trying not to be able to repeat the operators
   return (
-    <div style={{
-      marginLeft: "40%",
-      marginTop: "10%",
-      border: "5px solid black",
-      display: "flex",
-      minWidth: "300px",
-      width: "15%",
-      flexWrap: "wrap",
-      height: "400px",
-      borderRadius: "27px"
-    }}>
-      <Screen 
-      values={state}
-      />
-      <Keyboard 
-      addNumber={addNumber} 
-      />
+    <div className={styles.container}>
+      <Screen values={state} />
+      <Keyboard addNumber={addNumber} />
       <Operators
-      solve={solve}
-      removeNumber={removeNumber}
-      addNumber={addNumber} 
-      /> 
+        addOperator={addOperator}
+        solve={solve}
+        removeNumber={removeNumber}
+        addNumber={addNumber}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
