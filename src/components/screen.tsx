fdbf660279b619efea59cbcfb6ec6c83f1347d4c
Javier Perez <125394473+Javier0003@users.noreply.github.com>
2023-10-10 15:33:21 +0200

    added css file for the screen

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,22 +1,12 @@
-import { inputsProps } from "../types/inputs"
+import { inputsProps } from "../types/inputs";
+import styles from "../styles/screen.module.css"
 
-export function Screen( { values }: inputsProps) {
-    return(
-        <div style={{
-            border: "4px solid black",
-            width: "100%",
-            height: "20%",
-            borderRadius: "20px 20px 0px 0px",
-            backgroundColor: "black",
-        }}>
-            <div style={{
-                display: "flex",
-                color: "white",
-                justifyContent: "flex-end",
-                marginTop: "15px",
-                marginRight: "20px",
-                fontSize: "40px",
-            }}>{values}</div>
-        </div>
-    )
+export function Screen({ values }: inputsProps) {
+  return (
+    <div className={styles.screen}>
+      <div className={styles.numbers}>
+        {values}
+      </div>
+    </div>
+  );
 }
